6cf9b2888410e3b489b5e986acdb76f69e3b828f
Robert <fishwickrob@live.com>
2018-05-30 14:36:56 +0100

    trying to grab all notes

--- js/notepad.js
+++ js/notepad.js
@@ -3,11 +3,20 @@ function Notes () {
 }
 
 Notes.prototype.allNotes = function () {
-  // display list of notes
+  for(i=0; i=this.notesArray.length(); i++){
+    var node = document.createElement('LI');
+    var textnode = document.createTextNode(this.notesArray[i].content);
+    node.appendChild(textnode);
+    document.getElementById('snippets').appendChild(node);
+    console.log(this.notesArray);
+  }
 }
 
 Notes.prototype.addNote = function () {
-  this.notesArray.push({ 'content': document.getElementById('new_note').value })
+  this.notesArray.push({ 'content': document.getElementById('new_note').value });
+  this.allNotes();
+
 }
+
 
 var notes = new Notes()
