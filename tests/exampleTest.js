69efabdb27fba2fdeb121646fe9cefd03f06d403
benfurber <ben@furber-consultancy.com>
2018-05-30 16:18:05 +0100

    Refactored test

--- tests/exampleTest.js
+++ tests/exampleTest.js
@@ -1,26 +1,24 @@
+var testReturnValues = new TestReturnValues()
+
 function assertionIsTrue() {
-  var testReturnValues = new TestReturnValues()
   assert.isTrue(testReturnValues.trueStatement == true)
 };
 
 assertionIsTrue()
 
 function resultIsEqual() {
-  var testReturnValues = new TestReturnValues()
   assert.isEqual(testReturnValues.equalStatement, "test string")
 };
 
 resultIsEqual()
 
 function objectIsEmpty() {
-  var testReturnValues = new TestReturnValues()
   assert.isEmpty(testReturnValues.emptyObject)
 };
 
 objectIsEmpty()
 
 function objectDoesContain() {
-  var testReturnValues = new TestReturnValues()
   assert.contains(testReturnValues.notEmptyObject, "dog")
 };
 
