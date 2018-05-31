49624d38daa5efc47f928f81e2923f54f07d526e
Igor Ryabchuk <riabtchuk@gmail.com>
2018-05-31 12:35:21 +0100

    implements 'describe' and 'it' functions'

---
+++ spec/exampleSpec.js
@@ -0,0 +1,15 @@
+describe('A sample test', function () {
+  var testReturnValues = new TestReturnValues()
+  it('contains dog (contains test)', function () {
+    assert.contains(testReturnValues.notEmptyObject, "dog")
+  })
+  it('is empty (empty test)', function () {
+    assert.isEmpty(testReturnValues.emptyObject)
+  })
+  it('is equal to expected (equality test)', function () {
+    assert.isEqual(testReturnValues.equalStatement, "test string")
+  })
+  it('is true-ish (the true test)', function () {
+    assert.isTrue(testReturnValues.trueStatement == true)
+  })
+})
